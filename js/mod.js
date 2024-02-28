c1e7bfd3391963f48ce6d3f2ba025aff1d0be248
elisvoboda42066 <elisvoboda151@gmail.com>
2024-02-28 13:23:52 -0600

    update 1

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "First update",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
